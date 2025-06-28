142d085d8037091ee670e7588fdf94b55a429fbc
Aman0474 <117065032+Aman0474@users.noreply.github.com>
2025-06-29 02:39:17 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,7 +30,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/13G-euthralUzRc76qDATJLsNgSMAG6Jv/view?usp=drive_link"
+                href="https://drive.google.com/file/d/13G-euthralUzRc76qDATJLsNgSMAG6Jv/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
